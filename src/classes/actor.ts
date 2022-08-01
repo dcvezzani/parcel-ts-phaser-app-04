73b70458252f7009c88ddb6089224be9d3abd124
David Vezzani <dcvezzani@churchofjesuschrist.org>
2022-07-31 21:02:33 -0600

    Lesson 4

--- src/classes/actor.ts
+++ src/classes/actor.ts
@@ -1,6 +1,12 @@
 import Phaser from 'phaser';
+import { GameObjects, Physics, Input } from 'phaser';
 
 export default class Actor extends Phaser.Physics.Arcade.Sprite {
+    private _scale: number;
+    private debugOrigin!: GameObjects.Arc;
+    private debugBoundingBoxOrigin!: GameObjects.Arc;
+    private keySpace: Input.Keyboard.Key;
+
     constructor(
         scene: Phaser.Scene,
         spriteName: string,
@@ -10,20 +16,122 @@ export default class Actor extends Phaser.Physics.Arcade.Sprite {
     ) {
         super(scene, position.x, position.y, spriteName);
 
+        this._scale = scale;
+
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
-        this.setScale(scale);
+        if (gravity != undefined) this.setGravityY(gravity);
+        // this.setGravityY(-300);
 
-        const startingVelocityXValues: number[] = [-200, 200];
-        const randomStartingVelocityX: number =
-            startingVelocityXValues[Math.floor(Math.random() * startingVelocityXValues.length)];
-        this.setVelocity(randomStartingVelocityX, 200);
         this.setBounce(1);
         this.setCollideWorldBounds(true);
 
-        if (gravity != undefined) this.setGravityY(gravity);
+        const startingVelocity: Phaser.Math.Vector2 = this.getStartingVelocity();
+        this.setVelocity(startingVelocity.x, startingVelocity.y);
+        this._setScale(this._scale);
+
+        this.debugCreateOriginPoints();
+
+        this.keySpace = this.scene.input.keyboard.addKey(32);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        this.keySpace.on('down', (event: KeyboardEvent) => {
+            this.setVelocity(-this.getBody().velocity.x, this.getBody().velocity.y);
+            // this.scaleX = -this.scaleX;
+            // if (this.scaleX > 0) this.getBody().setOffset(0, 0);
+            // else this.getBody().setOffset(this.width, 0);
+            // this.showPoints();
+        });
+
+        this.on('destroy', () => {
+            this.keySpace.removeAllListeners();
+        });
     }
 
-    create(): void {}
+    update(): void {
+        this.checkFlip();
+
+        this.debugShowOriginPoints();
+    }
+
+    private getStartingVelocity(): Phaser.Math.Vector2 {
+        const startingVelocityXValues: number[] = [-200, 200];
+        const randomStartingVelocityX: number =
+            startingVelocityXValues[Math.floor(Math.random() * startingVelocityXValues.length)];
+
+        return new Phaser.Math.Vector2(randomStartingVelocityX, 200);
+    }
+
+    private _setScale(scale: number): void {
+        if (this.getBody().velocity.x > 0) this.setScale(-scale, Math.abs(scale));
+        else this.setScale(scale, Math.abs(scale));
+
+        // if (this.scaleX < 0) this.getBody().setOffset(this.width, 0);
+    }
+
+    private checkFlip(): void {
+        this.setDirection(this.body.velocity.x);
+    }
+
+    private moveLeft(): void {
+        this.scaleX = -this._scale;
+    }
+
+    private moveRight(): void {
+        this.scaleX = this._scale;
+    }
+
+    private showPoints(): void {
+        console.log('>>>positions', { x: this.x, y: this.y }, this.getBody().position);
+        console.log(
+            '>>>dimensions',
+            { width: this.width * Math.abs(this._scale), height: this.height * Math.abs(this._scale) },
+            { width: this.getBody().width, height: this.getBody().height },
+        );
+    }
+
+    private setDirection(velocity: number): void {
+        if (velocity < 0) {
+            this.moveLeft();
+        } else {
+            this.moveRight();
+        }
+
+        if (this.scaleX > 0) this.getBody().setOffset(0, 0);
+        else this.getBody().setOffset(this.width, 0);
+    }
+
+    private debugCreateOriginPoints(): void {
+        if (this.scene && this.scene.game.config.physics.arcade?.debug) {
+            this.debugOrigin = this.scene.add.circle(
+                this.x + this.originX,
+                this.y + this.originY,
+                Math.abs(10 * this._scale),
+                0x00ff00,
+            );
+            this.debugBoundingBoxOrigin = this.scene.add.circle(
+                this.getBody().x + this.getBody().halfWidth,
+                this.getBody().y + this.getBody().halfHeight,
+                Math.abs(10 * this._scale),
+                0xff0000,
+            );
+        }
+    }
+
+    private debugShowOriginPoints(): void {
+        if (this.scene && this.scene.game.config.physics.arcade?.debug) {
+            const debugOriginPosition = { x: this.x + this.originX, y: this.y + this.originY };
+            const debugBoundingBoxOrigin = {
+                x: this.getBody().x + this.getBody().halfWidth,
+                y: this.getBody().y + this.getBody().halfHeight,
+            };
+
+            this.debugOrigin.setPosition(debugOriginPosition.x, debugOriginPosition.y);
+            this.debugBoundingBoxOrigin.setPosition(debugBoundingBoxOrigin.x, debugBoundingBoxOrigin.y);
+        }
+    }
+
+    private getBody(): Physics.Arcade.Body {
+        return this.body as Physics.Arcade.Body;
+    }
 }
--- src/scenes/MyScene.ts
+++ src/scenes/MyScene.ts
@@ -2,8 +2,10 @@ import Phaser from 'phaser';
 import Actor from '~/classes/actor';
 
 export default class MyScene extends Phaser.Scene {
+    private actors: Actor[];
     constructor() {
         super('my-scene');
+        this.actors = [];
     }
 
     preload(): void {
@@ -11,16 +13,25 @@ export default class MyScene extends Phaser.Scene {
 
         this.load.image('cyborg', 'sprites/teen-titans-cyborg.png');
         this.load.image('robin', 'sprites/teen-titans-robin.png');
+        this.load.image('starfire', 'sprites/teen-titans-starfire.png');
     }
 
     create(): void {
-        const cyborg = new Actor(this, 'cyborg', new Phaser.Math.Vector2(100, 100), 0.75);
+        const cyborg = new Actor(this, 'cyborg', new Phaser.Math.Vector2(200, 200), -0.75);
         const robins = [
-            new Actor(this, 'robin', new Phaser.Math.Vector2(300, 300), 0.5, 500),
-            new Actor(this, 'robin', new Phaser.Math.Vector2(400, 400), 0.5, 500),
-            new Actor(this, 'robin', new Phaser.Math.Vector2(500, 500), 0.5, 500),
+            new Actor(this, 'robin', new Phaser.Math.Vector2(300, 300), -0.5, 500),
+            new Actor(this, 'robin', new Phaser.Math.Vector2(400, 400), -0.5, 500),
+            new Actor(this, 'robin', new Phaser.Math.Vector2(500, 500), -0.5, 500),
         ];
+        const starfire = new Actor(this, 'starfire', new Phaser.Math.Vector2(400, 200), 0.5, 700);
 
-        this.physics.add.collider([cyborg, ...robins], [cyborg, ...robins]);
+        this.actors = [cyborg, ...robins, starfire];
+        // this.actors = [cyborg];
+
+        this.physics.add.collider(this.actors, this.actors);
+    }
+
+    update(): void {
+        this.actors.forEach((actor) => actor.update());
     }
 }
